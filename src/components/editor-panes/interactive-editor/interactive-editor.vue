7f376a8bc11ae19b26a1bb8858f51496feb4107c
Quan Pham <doraemonfanclub@gmail.com>
2021-03-17 10:52:19 +0700

    Add proper toolbar icons

--- src/components/editor-panes/interactive-editor/interactive-editor.vue
+++ src/components/editor-panes/interactive-editor/interactive-editor.vue
@@ -4,6 +4,31 @@
       <div class="reveal">
         <div class="slides">
           <section>
+            <h2>Tablas con información</h2>
+            <i class="fa" :class="`fa-images`"></i>
+            <table>
+              <thead>
+                <tr>
+                  <th>Ciudad</th>
+                  <th>Año de fundación</th>
+                  <th>Población</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Gijón</td>
+                  <td>Siglo V a. C.</td>
+                  <td>274290</td>
+                </tr>
+                <tr>
+                  <td>Oviedo</td>
+                  <td>761</td>
+                  <td>221870</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+          <section>
             <h2>{{ title }}</h2>
             <h2></h2>
           </section>
--- src/components/editor-toolbar/editor-toolbar.vue
+++ src/components/editor-toolbar/editor-toolbar.vue
@@ -1,12 +1,28 @@
 <template>
   <el-row class="toolbar-wrapper">
-    <el-button size="medium"><i class="el-icon-plus" /> New Slide</el-button>
-    <el-button size="medium">
+    <el-button size="small">
+      <i class="el-icon-plus" />
+      New Slide
+    </el-button>
+
+    <el-button
+      v-for="(button, index) in editorButtons"
+      :key="index"
+      size="small"
+      type="text"
+      circle
+    >
+      <i class="fa" :class="`fa-${button.icon}`" />
+    </el-button>
+
+    <el-button size="small" type="warning">
       <i class="el-icon-s-platform" />
       Preview
     </el-button>
-    <el-button size="medium">Medium</el-button>
-    <el-button type="success" plain>Publish</el-button>
+    <el-button size="small" type="success" plain>
+      <i class="el-icon-upload2" />
+      Publish
+    </el-button>
   </el-row>
 </template>
 
@@ -15,11 +31,89 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "EditorToolbar",
+  data() {
+    return {
+      editorButtons: [
+        { icon: "bold", mode: "bold", action: "toggleBold", shortcut: "Ctrl-B", label: "Bold" },
+        {
+          icon: "italic",
+          mode: "italic",
+          action: "toggleItalic",
+          shortcut: "Ctrl-I",
+          label: "Italic",
+        },
+        {
+          icon: "strikethrough",
+          mode: "strikethrough",
+          action: "toggleStrikethrough",
+          label: "Strike Through",
+        },
+        {
+          icon: "heading",
+          mode: "heading",
+          action: "toggleHeadingSmaller",
+          shortcut: "Ctrl-H",
+          label: "Heading",
+        },
+        {},
+        {
+          icon: "code",
+          mode: "code",
+          action: "toggleCodeBlock",
+          shortcut: "Ctrl-Alt-C",
+          label: "Code",
+        },
+        {
+          icon: "quote-left",
+          mode: "quote",
+          action: "toggleBlockquote",
+          shortcut: "Ctrl-'",
+          label: "Quote",
+        },
+        {
+          icon: "list-ul",
+          mode: "unordered-list",
+          action: "toggleUnorderedList",
+          shortcut: "Ctrl-L",
+          label: "Genelic List",
+        },
+        {
+          icon: "list-ol",
+          mode: "ordered-list",
+          action: "toggleOrderedList",
+          shortcut: "Ctrl-Alt-L",
+          label: "Numbered List",
+        },
+        {},
+        {
+          icon: "link",
+          mode: "link",
+          action: "drawLink",
+          shortcut: "Ctrl-K",
+          label: "Create Link",
+        },
+        {
+          icon: "picture-o",
+          mode: "picture",
+          action: "drawImage",
+          shortcut: "Ctrl-Alt-I",
+          label: "Insert Image",
+        },
+        { icon: "table", mode: "table", action: "drawTable", label: "Insert Table" },
+        {
+          icon: "minus",
+          mode: "horizontal-rule",
+          action: "drawHorizontalRule",
+          label: "Insert Horizontal Rule",
+        },
+      ],
+    };
+  },
 });
 </script>
 
 <style lang="stylus" scoped>
 .toolbar-wrapper {
-  padding 6px 12px
+  padding 0px 12px
 }
 </style>
