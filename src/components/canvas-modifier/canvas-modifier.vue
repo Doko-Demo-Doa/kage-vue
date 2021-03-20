fb17635208b421a9afc2a6486eec0c57f1c33a91
Quan Pham <doraemonfanclub@gmail.com>
2021-03-20 10:45:57 +0700

    Add konva deps

---
+++ src/components/canvas-modifier/canvas-modifier.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="canvas-modifier">
+    <div class="counter">1</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Konva from "konva";
+
+@Options({
+  components: {
+    CanvasModifier,
+  },
+})
+export default class CanvasModifier extends Vue {
+  initializeKonva() {
+    const stage = new Konva.Stage({
+      container: "test",
+      width: 200,
+      height: 200,
+    });
+
+    const layer = new Konva.Layer();
+    stage.add(layer);
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.canvas-modifier
+  width 100%
+  height 100%
+</style>
